9f9cd255022ac592977c54fe984797852a1ad64a
BuildTools <unconfigured@null.spigotmc.org>
2023-03-03 18:12:53 +0100

    Setup

---
+++ src/main/java/de/sombeyyy/shops/ShopPlugin.java
@@ -0,0 +1,17 @@
+package de.sombeyyy.shops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShopPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
